fe18391ad0e89a9e1ceecdc4f1f818a0bef41b9b
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-04-22 14:22:38 -0400

    Updates

---
+++ precache-manifest.c39d0879fdd57d28615f7c2b7be9b239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f67840d2eb835ecb8c5",
+    "url": "/static/js/main.47c3e7ef.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2f67840d2eb835ecb8c5",
+    "url": "/static/css/main.7d4e63cf.chunk.css"
+  },
+  {
+    "revision": "ed4e2aaf5cd3e418e35d44307ef517f8",
+    "url": "/index.html"
+  }
+];
